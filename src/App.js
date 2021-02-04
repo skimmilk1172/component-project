ba2e1c642876de67cd3021cc494b06b5c033227c
Sean Kim <sean.sihun.kim@gmail.com>
2021-02-04 00:27:00 +0000

    add pictures and autoplay to 10 seconds, need to work on transition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 import Header from './components/Header';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
-function App() {
+const photos = [
+  {
+    name: 'Photo 1',
+    url: 'https://i.guim.co.uk/img/media/fbb5cc6e7642788d1b1fd2b0baab1641c06fc844/0_0_5760_3840/master/5760.jpg?width=700&quality=85&auto=format&fit=max&s=4accb731006caccd64710e863df7f5ad'
+  },
+  {
+    name: 'Photo 2',
+    url: 'https://static01.nyt.com/images/2019/12/12/arts/12ROCKY-1/merlin_165838272_2a037f9f-f1bf-4274-b5f6-0097bf644d92-mobileMasterAt3x.jpg'
+  },
+  {
+    name: 'Photo 3',
+    url: 'https://images.foxtv.com/static.fox13news.com/www.fox13news.com/content/uploads/2019/09/764/432/Getty_travisscott_111218_1542050256534_6390296_ver1.0_640_360.jpg?ve=1&tl=1'
+  }
+]
+
+
+
+class App extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      fade: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      arrows: false,
+      slidesToSroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      className: 'slides'
+    }
   return (
     <div className="App">
-      <Header pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <Header />
+      <Slider {...settings}>
+        {photos.map((photo) => {
+          return(
+            <div>
+              <img width='100%' src={photo.url} />
+            </div>
+          )
+        })}
+      </Slider>
     </div>
   );
+}
 }
 
 export default App;
